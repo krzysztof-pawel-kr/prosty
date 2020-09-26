8db833830ca8df52792c27cc5699725ba8ea67dd
Krzysztof Krupa <krzysztof.pawel.kr@gmail.com>
2020-09-26 19:52:52 +0200

    Kalkulator dziala

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 using namespace std;
 
+float a, b, wynik;
+
 int main(void)
 {
-    
+    cout << "Wpisz liczbe 1: ";
+    cin >> a;
+
+    cout << "Wpisz liczbe 2: ";
+    cin >> b;
+
+    cout << "Wynik dodawania wynosi: " << a+b << endl;  
 }
